271000f6201bb9c974faff079059605337d66e14
fukui-jin <jinfukui0130@icloud.com>
2021-02-19 01:47:55 +0900

    user.rb&テストコードの編集

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,30 +9,6 @@ RSpec.describe "Users", type: :request do
       it "nicknameとemail、passwordとpassword_confirmation、first_nameとlast_name、first_name_kanaとlast_name_kana、birthdayが存在すれば登録できる" do
         expect(@user).to be_valid
       end
-      it "passwordとpassword_confirmationが6文字以上であれば登録できる" do
-        @user.password = '000000'
-        @user.password_confirmation = '000000'
-      end
-      it "first_nameが全角であれば登録できる" do
-        @user.first_name = "全角"
-        @user.valid?
-      end
-      it "last_nameが全角であれば登録できる" do
-        @user.last_name = "全角"
-        @user.valid?
-      end
-      it "first_name_kanaが全角カタカナであれば登録できる" do
-        @user.first_name_kana = "カタカナ"
-        @user.valid?
-      end
-      it "last_name_kanaが全角カタカナであれば登録できる" do
-        @user.last_name_kana = "カタカナ"
-        @user.valid?
-      end
-      it "emailには@を含む必要がある" do
-        @user.email = '@'
-        @user.valid?
-      end
     end
     context '新規登録できないとき' do
       it "nicknameが空では登録できない" do
@@ -56,10 +32,28 @@ RSpec.describe "Users", type: :request do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it "passwordが5文字以下では登録できない" do
-        @user.password = "00000"
-        @user.password_confirmation = "00000"
+        @user.password = "00a00"
+        @user.password_confirmation = "00a00"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "passwordは英語のみでは登録できない" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it "passwordは数字のみでは登録できない" do
+        @user.password = "010101"
+        @user.password_confirmation = "010101"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it "passwordは全角では登録できない" do
+        @user.password = "全角全角全角"
+        @user.password_confirmation = "全角全角全角"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
       it "重複したemailが存在する場合登録できない" do
         @user.save
