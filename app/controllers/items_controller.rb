3752928cef5be0d1718235716f3b63bedb43ff4f
fukui-jin <jinfukui0130@icloud.com>
2021-02-21 04:57:45 +0900

    ActiveHashの適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,27 @@
 class ItemsController < ApplicationController
+
+  # authenticate_user!
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :description, :image, :category_id, :item_condition_id, :delivery_price_id, :prefectures_id, :delivery_time_id, ).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :orders
   # belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_price
+  belongs_to :prefectures
+  belongs_to :delivery_time
+
+  #カテゴリーの選択が「---」の時は保存できないようにする
+  validates :category_id, :delivery_price_id, :delivery_time_id, :item_condition_id, numericality: { other_than: 1 } 
+  validates :prefectures_id, numericality: { other_than: 0 } 
 end
