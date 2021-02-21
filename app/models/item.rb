5c20aee2a835f133c2aeb040a68e3e892fbd1fd8
fukui-jin <jinfukui0130@icloud.com>
2021-02-21 11:10:40 +0900

    モデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,17 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  has_many :orders
-  # belongs_to :user
+  # has_many :orders
+  belongs_to :user
   has_one_attached :image
+
+  #カテゴリーの選択が「---」の時は保存できないようにする
+  validates :category_id, :delivery_price_id, :delivery_time_id, :item_condition_id, numericality: { other_than: 1 } 
+  validates :prefectures_id, numericality: { other_than: 0 } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_condition
   belongs_to :delivery_price
   belongs_to :prefectures
   belongs_to :delivery_time
 
-  #カテゴリーの選択が「---」の時は保存できないようにする
-  validates :category_id, :delivery_price_id, :delivery_time_id, :item_condition_id, numericality: { other_than: 1 } 
-  validates :prefectures_id, numericality: { other_than: 0 } 
 end
