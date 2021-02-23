538b1cd6a50c0568f86e70283cc1b000fcb9a722
fukui-jin <jinfukui0130@icloud.com>
2021-02-23 20:18:00 +0900

    controllerの修正#3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :item_find_id, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless @item.user.id == current_user.id
       redirect_to action: :index
     end
