ac6132d81906edc4e2e601681be294843bedafc9
fukui-jin <jinfukui0130@icloud.com>
2021-02-25 18:26:03 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,34 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, expect: :index
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
     @order_buyer = OrderBuyer.new
   end
+  
 
   def create
+    @item = Item.find(params[:item_id])
     @order_buyer = OrderBuyer.new(order_params)
     if @order_buyer.valid?
+      pay_item
       @order_buyer.save
       redirect_to root_path
     else
-      render :new
+      render action: :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_buyer).permit(:user_id, :item_id, :postal_code, :prefectures, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id)
+    params.require(:order_buyer).permit(:postal_code, :prefectures_id, :city, :address, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_087fb5c063c2d868e77aa73f"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,15 +1,22 @@
 class OrderBuyer
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :city, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :city, :address, :building_name, :phone_number, :token, :price
 
   with_options presence: true do
-    validates :price, numericality:  {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :item_id
     validates :user_id
-    validates ::postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefectures_id
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/ }
+    validates :token
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validate :building_name
+  validates :prefectures_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    buyer = Buyer.create(price: price, user_id: user_id)
-
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Buyer.create(postal_code: postal_code, prefectures_id: prefectures_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
